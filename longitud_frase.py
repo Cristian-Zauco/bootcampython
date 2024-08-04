9adb3b474fcb7ebcb4de38cc3efd497c3be72782
Cristian-Zauco <czauco@gmail.com>
2024-08-03 20:36:56 -0600

    commit

---
+++ longitud_frase.py
@@ -0,0 +1,10 @@
+#Valida la longitud de una palabra 
+
+palabra = input("Introduce una frase: ")
+
+if len(palabra) >= 4 and len(palabra) <= 8:
+    print("La palabra " +palabra+ " es correcta")
+elif len(palabra) < 4:
+    print("Hacen falta letras. Solo tienen " + str(len(palabra))+ " letras" ) 
+elif len(palabra) > 8:
+    print("Sobran letras. Tiene " + str(len(palabra)) + " letras") 
