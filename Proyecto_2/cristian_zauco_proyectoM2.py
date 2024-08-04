e352ea36cd0692ae1145763ea24c3b83f1fb6a79
Cristian-Zauco <czauco@gmail.com>
2024-08-03 20:52:00 -0600

    commit

--- Proyecto_2/cristian_zauco_proyectoM2.py
+++ Proyecto_2/cristian_zauco_proyectoM2.py
@@ -1,6 +1,6 @@
 #Valida la longitud de una palabra 
 
-palabra = input("Introduce una frase: ")
+palabra = input("Introduce una palabra: ")
 
 if len(palabra) >= 4 and len(palabra) <= 8:
     print("La palabra " +palabra+ " es correcta")
@@ -8,3 +8,5 @@ elif len(palabra) < 4:
     print("Hacen falta letras. Solo tienen " + str(len(palabra))+ " letras" ) 
 elif len(palabra) > 8:
     print("Sobran letras. Tiene " + str(len(palabra)) + " letras") 
+
+
